cf180e692e5091398275aa001b220803bae17715
Thushan Subaskody <145941646+thushan7@users.noreply.github.com>
2023-09-25 20:56:31 -0400

    Update DNA.c

--- DNA.c
+++ DNA.c
@@ -1,32 +1,3 @@
-/********************subaskodyThushanA2.c*************************************
-Student Name: Thushan Subaskody     Email Id: tsubasko
-Due Date: Nov. 4, 2022              Course Name: CIS 1300
-
-I have exclusive control over this submission via my password.
-By including this statement in this header comment, I certify that:
-
-1) I have read and understood the University policy on academic integrity.
-2) I have completed the Computing with Integrity Tutorial on Moodle; and
-3) I have achieved at least 80% in the Computing with Integrity Self Test.
-
-I assert that this work is my own. I have appropriately acknowledged any and
-all material that I have used, whether directly quoted or paraphrased.
-Furthermore, I certify that this assignment was prepared by me specifically
-for this course.
-*****************************************************************************/
-
-/*****************************************************************************
-Compiling the program
-
-The program should be compiled using the following flags: -std=c99 -Wall
-
-Compiling:
-gcc -std=c99 -Wall subaskodyThushanA2.c main.c
-
-Running:
-./a.out
-*****************************************************************************/
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
